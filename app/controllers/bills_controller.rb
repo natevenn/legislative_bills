3c1f1b705ef8ad627860f6c34d5aaaee9474c0eb
Nathan Venn <natevenn@gmail.com>
2016-12-04 11:22:42 -0700

    Bill CRUD

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,22 +1,58 @@
 class BillsController < ApplicationController
   def new
+    @bill = Bill.new
+    @state = State.new
+    @action = Action.new
+    @category = Category.new
+    @sponsor = Sponsor.new
+    @party_affiliation = PartyAffiliation.new
   end
 
   def create
+    bill = Bill.create_with_associations(bill_params[:bill])
+    if bill.save
+      flash[:info] = "some message"
+      redirect_to bill_path(bill)
+    else
+      flash[:alert] = "some error message"
+      render :new
+    end
   end
 
   def show
+    @bill = Bill.find(params[:id])
   end
 
   def index
+    @bill = Bill.all
   end
 
   def edit
+    @bill = Bill.find(params[:id])
+    @action = @bill.actions
+    @category = @bill.categories
+    @sponsor = @bill.sponsors
+    @party_affiliation = @sponsor.party_affiliation
   end
 
   def update
+    bill = Bill.find(params[:id])
+    if bill.update_with_associations(bill_params[:bill])
+      flash[:info] = "some message"
+      redirect_to bill_path(bill)
+    else
+      flash[:alert] = "some error message"
+      render :edit
+    end
   end
 
-  def delete
+  def destroy
+    Bill.find(params[:id]).delete
   end
+
+  private
+
+    def bill_params
+      params.require(:bill).permit(:title, :description, state: [:name], action: [:name], category: [:name, :classification], sponsor: [:first_name, :last_name, party_affiliation: [:name]])
+    end
 end
--- app/models/bill.rb
+++ app/models/bill.rb
@@ -9,4 +9,41 @@ class Bill < ActiveRecord::Base
 
   validates :title, presence: true
   validates :description, presence: true
+
+  def self.create_bill_with_associations(params)
+    bill = Bill.create(params[:bill])
+    associate_with_state(bill, params)
+    associate_with_action(bill, params)
+    associate_with_categoties(bill, params)
+    associate_with_sponsors(bill, params)
+    bill
+  end
+
+  def self.associate_with_state(bill, params)
+    state = State.find_by(name: params[:state][:name])
+    bill.update(state_id: state.id)
+  end
+
+  def self.associate_with_action(bill, params)
+    #for multiple actions iterate over the array of actions
+    bill.actions.create(params[:action])
+  end
+
+  def self.associate_with_sponsor(bill, params)
+    #for multiple sponsors iterate over the array of sposnors
+    party = PartyAffiliation.create(params[:party_affiliation])
+    sponsor = bill.sponsors.create(params[:sponsor])
+    sponsor.update(party_affiliation_id: party.id)
+  end
+
+  def self.associate_with_categories(bill, params)
+    #for multiple categores iterate over the array of categores
+    bill.categories.create(params[:category])
+  end
+
+  def update_with_associations(params)
+    #find out what has changes i.e was it an action, category, new sponsor
+    # update the appropriate object by scoping from bill to that object
+    # if update success return true
+  end
 end
