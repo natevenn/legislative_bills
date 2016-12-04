5504448232eed5da76e5f2793ea53c842598908b
Nathan Venn <natevenn@gmail.com>
2016-12-04 13:58:16 -0700

    change route from /bill_updates to /updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-
-  get 'bills_updates/index'
-
   get 'home/index'
 
   resources :bills
+
+  get 'updates/index', to: 'bill_updates#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
